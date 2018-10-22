b474ebbc46870ec4799912ada74a90b5cb5731d5
luoyang <luoyang@eastmoney.com>
2018-10-23 00:24:21 +0800

    view base

--- NSTableViewWithXib/NSTableViewWithXib/AppDelegate.m
+++ NSTableViewWithXib/NSTableViewWithXib/AppDelegate.m
@@ -28,7 +28,6 @@
   @{@"name": @"Daba", @"address": @"Russia", @"gender": @"female", @"married": @ (1) }];
     
     [self.tableview reloadData];
-    
 }
 
 
@@ -49,6 +48,50 @@
 
 - (BOOL)tableView:(NSTableView *)tableView shouldEditTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
     return YES;
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    // 获取row数据
+    NSDictionary *data = _datas[row];
+    // 表格列的标识
+    NSString *identifier = tableColumn.identifier;
+    // 单元格数据
+    NSString *value  = data[identifier];
+    // 根据列的标识，创建单元格视图
+    NSView *view = [tableView makeViewWithIdentifier:identifier owner:self];
+    NSArray *subviews = [view subviews];
+    for (NSView *subview in subviews) {
+        if (![subview.identifier isEqualToString:identifier]) {
+//            continue;
+        }
+        if ([identifier isEqualToString:@"name"] || [identifier isEqualToString:@"address"]) {
+            NSTextField *textfield = (NSTextField *)subview;
+            // 更新单元格文本
+            if (value) {
+                textfield.stringValue = value;
+            }
+        }
+     
+        if ([identifier isEqualToString:@"gender"]) {
+            NSComboBox *combobox = (NSComboBox *)subview;
+            if (value) {
+                combobox.stringValue = value;
+            }
+            
+        }
+        
+        if ([identifier isEqualToString:@"married"]) {
+            NSButton *checkbutton = (NSButton *)subview;
+            if (value) {
+                checkbutton.state = [value boolValue];
+            }
+            
+        }
+        
+    }
+
+    return view;
+    
 }
 
 
